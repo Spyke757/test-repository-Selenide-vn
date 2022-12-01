43310648b70a9657880dbd7862725a0522d16053
Vladislav N <111731661+Spyke757@users.noreply.github.com>
2022-12-01 12:55:30 +0300

    Добавил page LaunchBrowser
    Добавил page LaunchBrowser

---
+++ java-example/src/main/java/LiteCartMagazine/LaunchBrowser.java
@@ -0,0 +1,53 @@
+package LiteCartMagazine;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+public class LaunchBrowser {
+
+    private static WebDriver driver;
+
+    @Before
+    public void setup(){
+//        System.setProperty("webdriver.chrome.driver", "C:\\Users\\vladislav.nazar\\Documents\\GitHub\\test-repository-Selenide-vn\\drivers\\chromedriver.exe");
+//        System.setProperty("webdriver.gecko.driver", "C:\\Users\\vladislav.nazar\\Documents\\GitHub\\test-repository-Selenide-vn\\drivers\\geckodriver.exe");
+          System.setProperty("webdriver.gecko.driver", "C:\\Users\\vladislav.nazar\\AppData\\Local\\Firefox Nightly\\firefox.exe");
+//        System.setProperty("webdriver.ie.driver","driver path\\IEDriverServer.exe");
+
+//        driver = new ChromeDriver();
+//        driver = new InternetExplorerDriver();
+
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public void LoginLiteCart() {
+        driver.get("http://localhost/litecart/admin/");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.name("username")).sendKeys("admin");;
+        driver.findElement(By.name("password")).sendKeys("admin");;
+        driver.findElement(By.xpath("//button[text()='Login']")).click();
+    }
+
+    @After
+    public void close(){
+        driver.quit();
+        driver = null;
+    }
+}
--- java-example/src/main/java/LiteCartMagazine/LoginLiteCart.java
+++ java-example/src/main/java/LiteCartMagazine/LoginLiteCart.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 import java.util.concurrent.TimeUnit;

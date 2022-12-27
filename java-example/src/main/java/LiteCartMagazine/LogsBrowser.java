aea3070db86618830187e48017cbf2df6f1e57fa
Vladislav N <111731661+Spyke757@users.noreply.github.com>
2022-12-27 10:10:09 +0300

    upd 17 task

--- java-example/src/main/java/LiteCartMagazine/LogsBrowser.java
+++ java-example/src/main/java/LiteCartMagazine/LogsBrowser.java
@@ -8,32 +8,26 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.logging.LogEntries;
-import org.openqa.selenium.logging.LogEntry;
-import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.CapabilityType;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 
 
-public class NoLogsMessages { //Task 17
+public class LogsBrowser { //Task 17
     private WebDriver driver;
     @Before
     public void setup() {
         System.setProperty("webdriver.chrome.driver", "C:\\tools\\drivers\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        LoggingPreferences prefs = new LoggingPreferences();
+        prefs.enable("browser", Level.ALL);
         ChromeOptions options = new ChromeOptions();
-        DesiredCapabilities caps = new DesiredCapabilities(options);
-        LoggingPreferences logPrefs = new LoggingPreferences();
-        logPrefs.enable(LogType.PERFORMANCE, Level.ALL);
-        caps.setCapability(ChromeOptions.LOGGING_PREFS, logPrefs);
-        options.setCapability(ChromeOptions.LOGGING_PREFS, caps);
+        options.setCapability(CapabilityType.LOGGING_PREFS, prefs);
+        driver = new ChromeDriver(options);
 
     }
     @Test
@@ -43,21 +37,16 @@ public class NoLogsMessages { //Task 17
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.xpath("//button[text()='Login']")).click();
+        System.out.println(driver.manage().logs().getAvailableLogTypes());
+        driver.manage().logs().get("browser").forEach(l-> System.out.println(l));
         List<WebElement> links = driver.findElements(By.xpath("//table[@class='dataTable']//td[3]/a"));
         for (int i = 0; i < links.size(); i++) {
             List<WebElement> product = driver.findElements(By.xpath("//table[@class='dataTable']//td[3]/a"));
             product.get(i).click();
+            driver.manage().logs().get("browser").forEach(l-> System.out.println(l));
             driver.manage().logs().get("browser")
                     .getAll().forEach(logEntry -> System.out.println("BROWSER: " + logEntry));
-
             driver.navigate().back();
-        }
-    }
-    public void analyzeLog() {
-        LogEntries logEntries = driver.manage().logs().get(LogType.BROWSER);
-        for (LogEntry entry : logEntries) {
-            System.out.println(new Date(entry.getTimestamp()) + " " + entry.getLevel() + " " + entry.getMessage());
-            //do something useful with the data
         }
     }
         @After
